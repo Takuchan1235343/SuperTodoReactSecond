71b1f04d1a88519fcd659215c099536b5bcb8c4b
kawataku <kawataku0819@gmail.com>
2024-06-08 15:03:41 +0900

    App周りのHookに書き出し

--- src/App.jsx
+++ src/App.jsx
@@ -1,54 +1,12 @@
-import React, { useState , useEffect} from 'react';
-import { auth } from './firebaseConfig';
-import {
-  onAuthStateChanged,
-  createUserWithEmailAndPassword,
-  signInWithEmailAndPassword,
-  signOut
-} from 'firebase/auth';
+import React, { useState } from 'react';
 import Todo from './Todo';
+import { useAuth } from './hooks/useAuth';
 
 function App() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-  const [user, setUser] = useState(null);
-  const [error, setError] = useState('');
 
-  useEffect(() => onAuthStateChanged(auth, (user) => setUser(user)), [])
-
-  const handleSignUp = async () => {
-    setError('');
-    try {
-      const userCredential = await createUserWithEmailAndPassword(auth, email, password);
-      setUser(userCredential.user);
-    } catch (error) {
-      if (error.code === 'auth/email-already-in-use') {
-        setError('このメールアドレスはすでに使用されています。');
-      } else {
-        setError(error.message);
-      }
-    }
-  };
-
-  const handleSignIn = async () => {
-    setError('');
-    try {
-      const userCredential = await signInWithEmailAndPassword(auth, email, password);
-      setUser(userCredential.user);
-    } catch (error) {
-      if (error.code === 'auth/user-not-found') {
-        setError('ユーザーが見つかりません。');
-      } else if (error.code === 'auth/wrong-password') {
-        setError('パスワードが正しくありません。');
-      } else {
-        setError(error.message);
-      }
-    }
-  };
-
-  const handleSignOut = async () => {
-    await signOut(auth);
-  };
+  const { signUp, signIn, signOut, user, errorMessage } = useAuth();
 
   return (
     <div>
@@ -57,7 +15,7 @@ function App() {
           <div className="todo-container">
             <div className="welcome-text">Welcome, {user.email}</div>
             <div className="todo-actions">
-              <button onClick={handleSignOut}>Sign Out</button>
+              <button onClick={signOut}>Sign Out</button>
             </div>
           </div>
           <div className="todo-content">
@@ -79,14 +37,15 @@ function App() {
             onChange={(e) => setPassword(e.target.value)}
           />
           <div>
-            <button onClick={handleSignIn}>Sign In</button>
+            <button onClick={() => signIn(email, password)}>Sign In</button>
             <button
               className="sign-up"
-              onClick={handleSignUp}
-            >Sign Up
+              onClick={() => signUp(email, password)}
+            >
+              Sign Up
             </button>
           </div>
-          {error && <p className="error-message">{error}</p>}
+          {errorMessage && <p className="error-message">{errorMessage}</p>}
         </div>
       )}
     </div>
