d6411499b765d34958d5322b090b9ebc6209e2c8
kawataku <kawataku0819@gmail.com>
2024-06-09 14:15:25 +0900

    IncompleteTaskのリファクタリング

--- src/components/CompleteTaskList.jsx
+++ src/components/CompleteTaskList.jsx
@@ -1,4 +1,5 @@
 import { Button } from './base/Button'
+import { incompleteTask } from '../hooks/useTasks'
 
 const style = {
   backgroundColor: '#aacfd0',
@@ -11,20 +12,31 @@ const style = {
 }
 
 export const CompleteTaskList = (props) => {
-  const { tasks, onClickBack } = props
+  const { tasks } = props
   return (
     <div style={style}>
       <p className="title">完了のTODO</p>
       <ul>
         {tasks.map((task) => (
-          <li key={task.id}>
-            <div className="list-row">
-              <p className="todo-item">{task.title}</p>
-              <Button onClick={() => onClickBack(task.id)}>戻す</Button>
-            </div>
-          </li>
+          < Task
+            key={task.id}
+            task={task}
+          />
         ))}
       </ul>
     </div>
+  )
+}
+
+//Liのコンポーネント化
+
+const Task = (props) => {
+  const { task } = props
+
+  return (
+    <li className="list-row">
+      <p className="todo-item">{task.title}</p>
+      <Button onClick={() => incompleteTask(task.id)}>戻す</Button>
+    </li>
   )
 }
