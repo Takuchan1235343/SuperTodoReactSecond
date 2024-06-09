91c3f612f20c58fc93c6d8d0bc701b83f29c4994
kawataku <kawataku0819@gmail.com>
2024-06-09 13:03:37 +0900

    InCompleteTaskListの修正

--- src/components/InCompleteTaskList.jsx
+++ src/components/InCompleteTaskList.jsx
@@ -1,4 +1,5 @@
 import { Button } from './base/Button'
+import { completeTask, removeTask } from '../hooks/useTasks'
 
 const style = {
   border: '2px solid #aacfd0',
@@ -11,21 +12,34 @@ const style = {
 }
 
 export const InCompleteTaskList = (props) => {
-  const { tasks, onClickComplete, onClickDelete } = props
+  const { tasks } = props
   return (
     <div style={style}>
       <p className="title">未完了のTODO</p>
       <ul>
         {tasks.map((task) => (
-          <li key={task.id}>
-            <div className="list-row">
-              <p className="todo-item">{task.title}</p>
-              <Button onClick={() => onClickComplete(task.id)}>完了</Button>
-              <Button onClick={() => onClickDelete(task.id)}>削除</Button>
-            </div>
-          </li>
+          <Task
+            key={task.id}
+            task={task}
+          />
         ))}
       </ul>
     </div>
+  )
+}
+
+//Liをコンポーネント化
+
+const Task = (props) => {
+  const { task } = props
+
+  return (
+    < li>
+      < div className="list-row">
+        <p className="todo-item">{task.title}</p>
+        <Button onClick={() => completeTask(task.id)}>完了</Button>
+        <Button onClick={() => removeTask(task.id)}>削除</Button>
+      </div>
+    </li>
   )
 }
