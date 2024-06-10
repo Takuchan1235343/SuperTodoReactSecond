a9fa94bc170172e549853062e6a936c3c6e196b1
kawataku <kawataku0819@gmail.com>
2024-06-10 12:08:47 +0900

    CompleteTaskList.jsxとTaskのTailwind化

--- src/components/CompleteTaskList.jsx
+++ src/components/CompleteTaskList.jsx
@@ -2,20 +2,10 @@ import { Button } from './base/Button'
 import { incompleteTask } from '../hooks/useTasks'
 import { Title } from './base/Title'
 
-const style = {
-  backgroundColor: '#aacfd0',
-  width: '400px',
-  minHeight: '200px',
-  padding: '8px',
-  margin: '8px',
-  borderRadius: '8px',
-
-}
-
 export const CompleteTaskList = (props) => {
   const { tasks } = props
   return (
-    <div style={style}>
+    <div className="bg-emerald-200 w-[400px] min-h-[200px] p-2 m-2 rounded-lg">
       <Title>完了のTODO</Title>
       <ul>
         {tasks.map((task) => (
@@ -35,8 +25,8 @@ const Task = (props) => {
   const { task } = props
 
   return (
-    <li style={{ display: 'flex', alignItems: 'center' }}>
-      <p style={{ margin: '6px' }}>{task.title}</p>
+    <li className="flex align-middle">
+      <p className="m-1.5">{task.title}</p>
       <Button onClick={() => incompleteTask(task.id)}>戻す</Button>
     </li>
   )
