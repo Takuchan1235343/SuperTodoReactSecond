ae6e87fe0194c3f8ee07a46df6ec376e9b60a2fa
takuchan <kawataku0819@gmail.com>
2024-06-13 08:57:40 +0900

    追加欄をエンターでサブミットできるように修正

--- src/components/CreateTaskForm.jsx
+++ src/components/CreateTaskForm.jsx
@@ -1,34 +1,37 @@
-import { useState } from 'react'
-import { Button } from './base/Button'
-import { Input } from './base/Input'
-import { createTask } from '../hooks/useTasks'
+import {useState} from 'react'
+import {Button} from './base/Button'
+import {Input} from './base/Input'
+import {createTask} from '../hooks/useTasks'
 
 export const CreateTaskForm = (props) => {
-  const { disabled } = props
+    const {disabled} = props
 
-  const [todoText, setTodoText] = useState('')
+    const [todoText, setTodoText] = useState('')
 
-  const handleChangeInput = (e) => setTodoText(e.target.value)
-  const handleClickAdd = async () => {
-    if (todoText === '') return
+    const handleChangeInput = (e) => setTodoText(e.target.value)
+    const handleClickAdd = async () => {
+        if (todoText === '') return
 
-    await createTask(todoText)
-    setTodoText('')
-  }
+        await createTask(todoText)
+        setTodoText('')
+    }
 
-  return (
-    <div className="bg-amber-200 w-full max-w-4xl min-w-96  flex flex-col md:flex-row justify-around p-2  m-2 items-center rounded-lg whitespace-nowrap">
-      <Input
-        disabled={disabled}
-        placeholder="Todoを入力"
-        value={todoText}
-        onChange={handleChangeInput}
-      />
-      <Button
-        disabled={disabled}
-        onClick={handleClickAdd}
-      >追加</Button>
-      {/*{disabled && (<p className="text-red-600">登録できるTODOは５個まで</p>)}*/}
-    </div>
-  )
+
+    return (
+        <div
+            className="bg-amber-200 w-full max-w-4xl min-w-96  flex flex-col md:flex-row justify-around p-2  m-2 items-center rounded-lg whitespace-nowrap">
+            <Input
+                disabled={disabled}
+                placeholder="Todoを入力"
+                value={todoText}
+                onChange={handleChangeInput}
+                onEnter={handleClickAdd}
+            />
+            <Button
+                disabled={disabled}
+                onClick={handleClickAdd}
+            >追加</Button>
+            {/*{disabled && (<p className="text-red-600">登録できるTODOは５個まで</p>)}*/}
+        </div>
+    )
 }
--- src/components/base/Input.jsx
+++ src/components/base/Input.jsx
@@ -1,14 +1,21 @@
 export const Input = (props) => {
-  const { value, onChange, placeholder, type, disabled } = props
+    const {value, onChange, placeholder, type, disabled, onEnter} = props
 
-  return (
-    <input
-      // disabled={disabled}
-      className="border-none min-w-96 text-center rounded-lg p-1.5 m-2"
-      type={type}
-      value={value}
-      onChange={onChange}
-      placeholder={placeholder}
-    />
-  )
+    const handleKeyDown = (e) => {
+        if (e.key === 'Enter') {
+            onEnter()
+        }
+    }
+
+    return (
+        <input
+            // disabled={disabled}
+            className="border-none min-w-96 text-center rounded-lg p-1.5 m-2"
+            type={type}
+            value={value}
+            onChange={onChange}
+            placeholder={placeholder}
+            onKeyDown={handleKeyDown}
+        />
+    )
 }
